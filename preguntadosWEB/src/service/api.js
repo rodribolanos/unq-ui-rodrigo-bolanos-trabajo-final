02481fac2224e99a92efe6c31b153e8b29e34858
rodribolanos <rodrimanuelbw@gmail.com>
2024-06-22 12:41:42 -0300

    API's implementation

---
+++ preguntadosWEB/src/service/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const urlAPI = 'https://preguntados-api.vercel.app';
+
+const get = (url) => axios.get(url);
+const post = (url, body) => axios.post(url, body);
+
+export const getDifficulty = () => get(urlAPI+'/difficulty?');
+export const getQuestions = (difficulty) => get(urlAPI+'/questions?difficulty='+difficulty);
+export const postAnswer = (body) => post(urlAPI+'/answer', body);
+
+// TODO tener en cuenta: El body esperable por la API para la answer es: { questionId, option }
