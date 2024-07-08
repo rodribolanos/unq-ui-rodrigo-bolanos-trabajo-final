da70f046a28db72885cc75563dcfa6981186e98d
Rodrigo Bolaños <rodrimanuelbw@gmail.com>
2024-07-07 22:02:37 -0300

    Innecesarios estilos

--- preguntadosWEB/src/views/Home/Home.jsx
+++ preguntadosWEB/src/views/Home/Home.jsx
@@ -1,6 +1,8 @@
 import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const randomColors  = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6']
+
     const navigate = useNavigate();
 
     const goDifficulties = () => {
@@ -11,6 +13,11 @@ const Home = () => {
         <div>
             <h1>Home</h1>
             <p>Welcome to PREGUNTADOS</p>
+            <p>
+                {randomColors.map((color, index) => (
+                    <span key={index} style={{color}}>{'PREGUNTADOS'[index]}</span>
+                ))}
+            </p>
             <div className='button-container'>
                 <button className='button' onClick={goDifficulties}>Start</button>
             </div>
